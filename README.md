632575cfd4f23f14077bf96d7c7bfd450ebe22f2
pdlawson1@gmail.com <pdlawson1@gmail.com>
2018-10-07 23:24:06 +0100

    WIP main endpoints

---
+++ README.md
@@ -0,0 +1,6 @@
+Setup:
+1) Add Betfair cresentials to keys/credentials in the following format
+username: <username>
+password: <password>
+2) Add self-signed certificate pem file (for the certificate linked to your Betfair account) to keys/cert-2048.pem
+3) Add your Betfair access token to keys/betfair_app_token
--- lib/api/client.rb
+++ lib/api/client.rb
@@ -12,21 +12,85 @@ module API
     base_uri "https://api.betfair.com/exchange/betting/rest/v1.0/"
     #debug_output $stdout
 
+    def self.list_event_types(market_filter = {})
+      request('listEventTypes', {filter: market_filter})
+    end
+
+    def self.list_competitions(market_filter = {})
+      request('listCompetitions', {filter: market_filter})
+    end
+
+    def self.list_events(market_filter = {})
+      request('listEvents', {filter: market_filter})
+    end
+
+    def self.list_market_types(market_filter = {})
+      request('listMarketTypes', {filter: market_filter})
+    end
+
+    def self.list_countries(market_filter = {})
+      request('listCountries', {filter: market_filter})
+    end
+
+    class MarketProjection
+      COMPETITION = 'COMPETITION'.freeze
+      EVENT = 'EVENT'.freeze
+      EVENT_TYPE = 'EVENT_TYPE'.freeze
+      MARKET_START_TIME = 'MARKET_START_TIME'.freeze
+      MARKET_DESCRIPTION = 'MARKET_DESCRIPTION'.freeze
+      RUNNER_DESCRIPTION = 'RUNNER_DESCRIPTION'.freeze # just for horses?
+      RUNNER_METADATA = 'RUNNER_METADATA'.freeze # just for horses?
+    end
+
+    class MarketSort
+      RANK = 'RANK'.freeze # black box sort based on lots of criteria
+      MINIMUM_TRADED = 'MINIMUM_TRADED'.freeze
+      MAXIMUM_TRADED = 'MAXIMUM_TRADED'.freeze
+      MINIMUM_AVAILABLE = 'MINIMUM_AVAILABLE'.freeze
+      MAXIMUM_AVAILABLE = 'MAXIMUM_AVAILABLE'.freeze
+      FIRST_TO_START = 'FIRST_TO_START'.freeze
+      LAST_TO_START = 'LAST_TO_START'.freeze
+    end
+
+    def self.list_market_catalogue(market_filter = {}, max_results = 100, market_projection = [], market_sort = nil)
+      raise 'max_results must be <= 1000' unless max_results <= 1000
+      options = {filter: market_filter, maxResults: max_results}
+      options[:marketProjection] = market_projection if market_projection.any?
+      options[:sort] = market_sort if market_sort
+      request('listMarketCatalogue', options)
+    end
+
+    class PriceProjection
+      def to_hash
+        {}
+      end
+    end
+
+    class OrderProjection
+    end
+
+    def self.list_market_book(market_ids = [], price_projection = nil, order_projection = nil)
+      options = {marketIds: market_ids.map(&:to_str)}
+      options[:priceProjection] = price_projection.to_hash if price_projection
+      options[:orderProjection] = order_projection if order_projection
+      request('listMarketBook', options)
+    end
+
+    private
+
+    def self.request(action, options = nil)
+      body = options ? options.to_json : ''
+      response = post("/#{action}/", body: body, headers: headers)
+      #TODO - error handling
+      response.parsed_response
+    end
+
     def self.session_token
       @session_token ||= begin
         API::LoginClient.log_in!
         API::LoginClient.session_token
       end
     end
-
-    def self.get_event_types
-      body = '{"filter":{}}'
-      response = post('/listEventTypes/', body: body, headers: headers)
-      data = response.parsed_response
-      pp data
-    end
-
-    private
 
     def self.headers
       {
